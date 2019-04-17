f72a039a55e0311f5bc6eacf47d426fe3d401542
leopizzi <leopizzi94@gmail.com>
2019-04-17 14:43:00 -0300

    Adicionado modulo de contagem de letras e palavras.

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,12 @@
 var frase = $(".frase").text();
 var numPalavras = frase.split(" ").length;
 var tamanhoFrase = $("#tamanho-frase");
-
 tamanhoFrase.text(numPalavras);
+
+var campo = $(".campo-digitacao");
+campo.on("input", function(){
+  var conteudo = campo.val();
+  var qtdPalavras = conteudo.split(" ").length;
+  $("#contador-palavras").text(qtdPalavras);
+  $("#contador-caracteres").text(conteudo.length);
+});
